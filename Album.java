a59b34c023004be8f52dff94a05b20585059d681
notkilometers <47023811+notkilometers@users.noreply.github.com>
2021-05-09 16:32:38 -0700

    added option to choose path

--- Album.java
+++ Album.java
@@ -11,6 +11,7 @@ public class Album
 {
     java.util.List<String> urls;
     String url;
+    String filepath;
     int count = 1;
 	
 	public static void main(String[] args) 
@@ -50,17 +51,24 @@ public class Album
 	public void genPage() 
         {
             Album t = this;
-            Frame f = new Frame("Button Ex");
-            Button b = new Button("Submit");
-            final TextField tf=new TextField();  
-            b.setBounds(50,100,80,30);
-            tf.setBounds(50,50, 150,20); 
+            Frame frame = new Frame("Imgur Album Download");
+            Font font = new Font("SansSerif",Font.BOLD, 28);
+            Button button = new Button("Download");
+            final TextField urlText= new TextField();
+            final TextField pathText= new TextField(); 
+            urlText.setFont(font);
+            pathText.setFont(font);
+            button.setBounds(290, 65, 100, 40);
+            urlText.setBounds(15, 45, 270, 40); 
+            pathText.setBounds(15, 90, 270, 40); 
             
-            b.addActionListener(new ActionListener(){  
+            button.addActionListener(new ActionListener(){  
             public void actionPerformed(ActionEvent e)
             {
-                url = tf.getText();
-                tf.setText("Processing...");
+                url = urlText.getText();
+                filepath = pathText.getText();
+                
+                urlText.setText("Processing...");
                     
                 t.getUrls();
                     
@@ -71,15 +79,16 @@ public class Album
                     downloadImg(s);
                 }
                     
-                tf.setText(""); 
+                urlText.setText(""); 
             }  
             });
             
-            f.add(b);
-            f.add(tf);
-            f.setSize(400,400);
-            f.setLayout(null);
-            f.setVisible(true);
+            frame.add(button);
+            frame.add(urlText);
+            frame.add(pathText);
+            frame.setSize(400,150);
+            frame.setLayout(null);
+            frame.setVisible(true);
         }
         
         private void downloadImg(String url) 
@@ -88,7 +97,7 @@ public class Album
             {
                 URL surl = new URL(url);
                 BufferedImage img = ImageIO.read(surl);
-                File file = new File("C:\\Users\\Miles\\Desktop\\java\\test\\" + count + "_" + url.split("/",-2)[3]);
+                File file = new File(filepath + count + "_" + url.split("/",-2)[3]);
                 ImageIO.write(img, "jpg", file);
                 count += 1;
             }
