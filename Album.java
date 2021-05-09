0c014a18356723c6cc83143d2e7e019cba54c2db
notkilometers <47023811+notkilometers@users.noreply.github.com>
2021-05-09 15:07:26 -0700

    added file dl

--- Album.java
+++ Album.java
@@ -1,44 +1,48 @@
+import com.google.common.io.Files;
 import java.util.*; 
+import java.awt.*;  
+import java.awt.event.*;  
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import javax.imageio.ImageIO;
 
 public class Album 
 {
-    List<String> urls;
+    java.util.List<String> urls;
     String url;
+    int count = 1;
 	
-	public static void main(String[] args) 
+	public static void main(String[] args) throws IOException 
 	{
-		Album t = new Album("https://imgur.com/gallery/lOlu2qV"); // test album
+		Album t = new Album();
 		
-		t.getUrls();
-                
-    t.printUrls();
-	}
-	
-	public Album(String findUrl)
-	{
-		url = findUrl;
+                t.genPage();
 	}
 	
 	public Album() {}
 	
-	public void getUrls()
+	private void getUrls()
 	{
-            Driver d = new Driver(url);
+            boolButton b = new boolButton(url);
                 
-            while(d.getbutton())
+            while(b.getbutton())
             {
-               d.click();
+               b.click();
             }
                 
-            if(d.getUrls())
+            if(b.getUrls())
             {
-                urls = d.urls;
+                urls = b.urls;
             }
             
-            d.driver.quit();
+            b.driver.quit();
 	}
         
-        public void printUrls()
+        private void printUrls()
         {
             for (String s : urls)
             {
@@ -46,5 +50,54 @@ public class Album
             }
         }
 	
-	
+	public void genPage() throws FileNotFoundException, IOException
+        {
+            Frame f = new Frame("Button Ex");
+            Button b = new Button("Submit");
+            
+            Album t = this;
+            
+            final TextField tf=new TextField();  
+            b.setBounds(50,100,80,30);
+            tf.setBounds(50,50, 150,20);  
+            b.addActionListener(new ActionListener(){  
+            public void actionPerformed(ActionEvent e)
+            {  
+                try {
+                    url = tf.getText();
+                    tf.setText("Processing...");
+                    
+                    t.getUrls();
+                    
+                    t.printUrls();
+                    
+                    for (String s : urls)
+                    {
+                        downloadImg(s);
+                    }
+                    
+                    tf.setText("");
+                    
+                } catch (Exception ee) {} 
+            }  
+            });
+            
+            f.add(b);f.add(tf);
+            f.setSize(400,400);
+            f.setLayout(null);
+            f.setVisible(true);
+        }
+        
+        private void downloadImg(String url) throws MalformedURLException, FileNotFoundException, IOException
+        {
+            try
+            {
+                URL surl = new URL(url);
+                BufferedImage img = ImageIO.read(surl);
+                File file = new File("C:\\Users\\Miles\\Desktop\\java\\test\\" + count + "_" + url.split("/",-2)[3]);
+                ImageIO.write(img, "jpg", file);
+                count += 1;
+            }
+            catch(Exception e) {}
+        }
 }
